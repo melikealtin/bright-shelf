3142974e82b79600038a6aaa221ff4a1b7d75312
melikealtin <melikealtin414@gmail.com>
2025-04-02 16:59:34 +0300

    Add auto navigation and tabs setup

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,9 +1,34 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import SafeScreen from "../components/SafeScreen";
 import { StatusBar } from "expo-status-bar";
 
+import { useAuthStore } from "../store/authStore";
+import { useEffect } from "react";
+
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+
+  const { checkAuth, user, token } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  useEffect(() => {
+    const inAuthScreen = segments[0] === "(auth)";
+    const isLoggedIn = user && token;
+
+    if (!inAuthScreen && !isLoggedIn) {
+      router.replace("/(auth)");
+    } else if (inAuthScreen && isLoggedIn) {
+      router.replace("/(tabs)");
+    }
+  }, [user, token, segments]);
+
+  console.log("segments", segments);
+
   return (
     <SafeAreaProvider>
       <SafeScreen>
@@ -12,7 +37,7 @@ export default function RootLayout() {
             headerShown: false,
           }}
         >
-          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
           <Stack.Screen name="(auth)" />
         </Stack>
       </SafeScreen>
